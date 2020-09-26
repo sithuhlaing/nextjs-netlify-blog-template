dc4fd64be8ed32b64f24c4a3e51b8a0075da3495
si thu hlaing <sithu.hlaing@allianz.com>
2020-09-26 17:42:11 +0700

    update 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Index() {
             Funcy <span className="fancy">.</span>
           </h1>
           <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <h2>OHHHHHH.........</h2>
           <SocialList />
         </div>
       </div>
